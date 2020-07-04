03abf7ae6cecdf03f3a49e5b0ad4523ed9ad3a3b
mrazali <mohdfadzli.razali@sicpa.com>
2020-07-05 00:25:31 +0800

    init project import all dependencies & plugins Added List of cat & cat detail fragments Added navigation link above screens

---
+++ app/src/main/java/com/inovasiti/kucingapp/view/CatFragment.kt
@@ -0,0 +1,51 @@
+package com.inovasiti.kucingapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.inovasiti.kucingapp.R
+import com.inovasiti.kucingapp.databinding.FragmentCatBinding
+import kotlinx.android.synthetic.main.fragment_cat.*
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CatFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CatFragment : Fragment() {
+
+    private var catUID = 0
+    private lateinit var dataBinding: FragmentCatBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_cat, container, false)
+        return dataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //if arg != null
+        arguments?.let {
+            catUID = CatFragmentArgs.fromBundle(it).catValue
+            textView2.text = "Args "+catUID.toString()
+        }
+
+        floatingActionButton2.setOnClickListener {
+            val action = CatFragmentDirections.actionCatFragmentToListFragment();
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
---
+++ app/src/main/java/com/inovasiti/kucingapp/view/ListFragment.kt
@@ -0,0 +1,45 @@
+package com.inovasiti.kucingapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.ListFragment
+import androidx.navigation.Navigation
+import com.inovasiti.kucingapp.R
+import com.inovasiti.kucingapp.databinding.FragmentListBinding
+import kotlinx.android.synthetic.main.fragment_list.*
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ListFragment : Fragment() {
+     private lateinit var dataBinding: FragmentListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false)
+        return dataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        floatingActionButton.setOnClickListener {
+            //ListFragmentDirections class came from gradle plugin: "androidx.navigation"
+            val action = ListFragmentDirections.actionListFragmentToCatFragment();
+            Navigation.findNavController(it).navigate(action)
+        }
+
+    }
+}
